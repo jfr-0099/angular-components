80b7783e031fd1115be6f7d3d575ef9cbe6b9377
Michael Rooney <msroone@gmail.com>
2026-03-14 14:57:01 -0500

    generate component

---
+++ src/app/mrooney-addition/mrooney-addition.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mrooney-addition',
+  imports: [],
+  templateUrl: './mrooney-addition.html',
+  styleUrl: './mrooney-addition.css',
+})
+export class MrooneyAddition {
+
+}
