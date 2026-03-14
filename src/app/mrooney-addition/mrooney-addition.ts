2b7f73fc8935275e3b3b6b7686d305d000a65dd1
Michael Rooney <msroone@gmail.com>
2026-03-14 15:08:23 -0500

    built structure and imported

--- src/app/mrooney-addition/mrooney-addition.ts
+++ src/app/mrooney-addition/mrooney-addition.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-mrooney-addition',
-  imports: [],
+  imports: [MatCardModule, MatProgressSpinnerModule],
   templateUrl: './mrooney-addition.html',
   styleUrl: './mrooney-addition.css',
 })
