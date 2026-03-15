a67199ded689f7b5121282e8561c1ca03fe47d40
lmeier127 <Lmeier127@gmail.com>
2026-03-15 14:25:26 -0500

    Added signals

--- src/app/lmeier-custom-topping/lmeier-custom-topping.ts
+++ src/app/lmeier-custom-topping/lmeier-custom-topping.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { MatAnchor, MatButtonModule } from "@angular/material/button";
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-lmeier-custom-topping',
   imports: [
-    MatCardModule
-  ],
+    MatCardModule,
+    MatAnchor,
+    MatButtonModule,
+    FormsModule,
+    MatInputModule
+],
   templateUrl: './lmeier-custom-topping.html',
   styleUrl: './lmeier-custom-topping.css',
 })
 export class LmeierCustomTopping {
 
+    protected readonly customTopping = signal("");
+    protected readonly customToppingList = signal("Custom Topping List");
 }
